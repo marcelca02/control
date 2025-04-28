679b38facf66e10eb4199500cf0bc3d32d010b96
marcelca02 <marcel.calleja@estudiantat.upc.edu>
2025-04-28 23:02:41 +0200

    add pid_controller fn

--- 2d_dron.py
+++ 2d_dron.py
@@ -19,9 +19,8 @@ Kp_y = 0.8
 Ki_y = 0.0001
 Kd_y = 3
 
-# Reference values
 x_ref = 0.0
-y_ref = 0.0
+y_ref = 10.0
 theta_ref = 0.0
 
 # Initial conditions
@@ -31,6 +30,26 @@ starting_theta=0.0
 starting_vx=0.0
 starting_vy=0.0
 starting_omega=0.0
+
+def pid_controller(theta, error_x, error_y, integral_error_y, derivative_error_y, previous_error_y):
+    integral_error_y += error_y * dt
+    derivative_error_y = (error_y - previous_error_y) / dt
+
+    theta_error = theta_ref - theta
+
+    torque = Kp_theta * theta_error
+    delta_F = torque * I / d    # Force imbalance between motors
+
+    Ftotal = Fg + (Kp_y * error_y) + (Ki_y * integral_error_y) + (Kd_y * derivative_error_y)
+
+    Fl = (Ftotal - delta_F) / 2
+    Fl = (Ftotal + delta_F) / 2
+
+    Fl = np.clip(Fl, 0, 100)
+    Fr = np.clip(Fl, 0, 100)
+
+    return Fl, Fr
+
 
 def simulate_system():
     x = starting_x
@@ -50,24 +69,10 @@ def simulate_system():
     previous_error_y = 0
 
     for _ in range(n_steps):
-
         error_x = x_ref - x
         error_y = y_ref - y
-        integral_error_y += error_y * dt
-        derivative_error_y = (error_y - previous_error_y) / dt
 
-        theta_error = theta_ref - theta
-
-        torque = Kp_theta * theta_error
-        delta_F = torque * I / d    # Force imbalance between motors
-
-        Ftotal = Fg + (Kp_y * error_y) + (Ki_y * integral_error_y) + (Kd_y * derivative_error_y)
-
-        Fl = (Ftotal - delta_F) / 2
-        Fl = (Ftotal + delta_F) / 2
-
-        Fl = np.clip(Fl, 0, 100)
-        Fr = np.clip(Fl, 0, 100)
+        Fl, Fr = pid_controller(theta, error_x, error_y, integral_error_y, derivative_error_y, previous_error_y)
 
         # Vertical movement
         ay = ((Fl + Fr)*math.cos(theta) - Fg)/m
@@ -101,21 +106,21 @@ x_positions, y_positions, _, _ = simulate_system()
 # Plotting
 fig, axs = plt.subplots(3, 1, figsize=(20, 12), sharex=True)
 
-axs[0].plot(time_steps, x_positions, label='X Position', color='blue')
+axs[0].plot(x_positions, time_steps, label='X Position', color='blue')
 axs[0].set_title('Dron Horizontal Position vs Time')
-axs[0].set_xlabel('Time (s)')
-axs[0].set_ylabel('X Position (m)')
+axs[0].set_xlabel('X Position (m)')
+axs[0].set_ylabel('Time (s)')
 axs[0].grid()
 axs[0].legend()
 
-axs[1].plot(time_steps, y_positions, label='Y Position', color='green')
+axs[1].plot(time_steps, y_positions, label='Y Position', color='red')
 axs[1].set_title('Dron Vertical Position vs Time')
 axs[1].set_xlabel('Time (s)')
 axs[1].set_ylabel('Y Position (m)')
 axs[1].grid()
 axs[1].legend()
 
-axs[2].plot(x_positions, y_positions, label='Dron Path', color='red')
+axs[2].plot(x_positions, y_positions, label='Dron Path', color='green')
 axs[2].set_title('Dron Path')
 axs[2].set_xlabel('X Position (m)')
 axs[2].set_ylabel('Y Position (m)')
